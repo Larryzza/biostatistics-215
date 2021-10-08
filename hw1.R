eb198491f4d89a0fe9aad893559747d902d61cd1
zianzhaung <zianzhaung@ucla.edu>
2021-10-08 11:04:38 -0700

    hw1

--- hw1.R
+++ hw1.R
@@ -1,7 +1,11 @@
+rm(list = ls())
+
 library(survival)
 library(KMsurv)
 library(tidyverse)
 data("drug6mp")
+
+# Q3.4 additional 2
 
 logn_fit <- survreg(Surv(t2, relapse) ~ 1, 
                     data = drug6mp, dist = "lognormal")
@@ -10,12 +14,19 @@ summary(logn_fit)
 mu    <- logn_fit$coefficients[1]
 sigma <- logn_fit$scale
 
-
-prob <- NULL
-set.seed(12345)
-for(i in 1:1000000){
-  sim_mu <- rnorm(1, mu, sqrt(logn_fit$var[1,1]))
-  sim_sigma <- rnorm(1, log(logn_fit$scale), sqrt(logn_fit$var[2,2])) %>% exp 
-  prob <- c(prob, 1-pnorm((log(12)-sim_mu)/sim_sigma))
+vol <- 10000000
+.sim_fun <- function(x){
+  prob <- 1-pnorm((log(12)-x[1])/x[2])
+  return(prob)
 }
+set.seed(19969)
+sim <- data.frame(sim_mu = rnorm(vol, mu, sqrt(logn_fit$var[1,1])),
+                  sim_sigma = rnorm(vol, log(logn_fit$scale), 
+                                    sqrt(logn_fit$var[2,2])) %>% exp)
+
+start_time <- Sys.time()
+prob <- apply(sim, 1, .sim_fun)
+end_time <- Sys.time()
+end_time - start_time
+
 quantile(prob, c(0.025, 0.975))
